7b93b62a2b0c4c2f44db3327b18e9028c3023c2f
vitaliy-tymashkov <vitaliy.tima@gmail.com>
2021-05-09 18:29:02 +0200

    Initial commit

---
+++ src/main/java/com/self/restserviceexample/RestServiceExampleApplication.java
@@ -0,0 +1,13 @@
+package com.self.restserviceexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceExampleApplication.class, args);
+	}
+
+}
